f932e448fe3de4b5c8bc3334b9256fd267c4928f
TsaiGai <iantsai11267@gmail.com>
2025-02-10 17:26:03 -0800

    Delete copies

--- renter-side/src/App/Dashboard.jsx
+++ renter-side/src/App/Dashboard.jsx
@@ -23,7 +23,7 @@ export default function Dashboard() {
       try {
         const token = await user.getIdToken();
 
-        const response = await fetch("http://localhost:5000/api/apartments", {
+        const response = await fetch("http://localhost:5000/apartments", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
